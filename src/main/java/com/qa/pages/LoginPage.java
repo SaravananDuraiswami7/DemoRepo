637b73f7bf6d55cde4bda5386cba76628f7c8d80
unknown <saravanan.kcd@gmail.com>
2019-06-11 09:53:16 +0530

    commit after add

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,10 +1,19 @@
 package com.qa.pages;
 
+import org.testng.annotations.Test;
+
 public class LoginPage {
 	
 	public void loginPage()
 	{
 		System.out.println("Login Page");
 	}
+	
+	@Test
+	public void LoginTest()
+	{
+		System.out.println("test1");
+	}
+
 
 }
